13366375834857539121f7aee486c2e3dc701211
Corentin de Maupeou <demaupeoucorentin@gmail.com>
2019-02-27 01:04:00 +0100

    maj

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function onDownload() {
+    alert('Vous allez être redirigé vers la page de téléchargement.')
+    window.open('https://mega.nz/#!yLImQITZ!zm3M-qE3hseTDfkL2QnLh00LZ7A3LZE_RaUrFANB_Aw')
+}
+
+function openSourceCode() {
+    window.open('https://github.com/donaldtrumpyyy/DEMJAR')
+}
