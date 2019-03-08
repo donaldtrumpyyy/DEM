f78bb68778bb27b0da031b37b71563790ac80a91
Corentin de Maupeou <demaupeoucorentin@gmail.com>
2019-03-08 22:18:00 +0100

    maj

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 function onDownload() {
     alert('You will be redirected to the download page.')
-    window.open('https://mega.nz/#!fG5jAazT!oMJu3Rj5z-gtEOhJjgRIcYSIMhIMcbYcsVQek7oA2qc')
+    window.open('https://mega.nz/#!XCZgBQKQ!Qv6z_PRFzAWSQq5XER_Uh8bcgvvMHE8pAlvGoU2lvZc')
 }
 
 function openSourceCode() {
