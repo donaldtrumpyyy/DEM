574f6e7145cd1b72a8b3b706d2bd6acf1b8ec5f8
Corentin de Maupeou <demaupeoucorentin@gmail.com>
2019-03-09 19:05:58 +0100

    maj

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 function onDownload() {
     alert('You will be redirected to the download page.')
-    window.open('https://mega.nz/#!XCZgBQKQ!Qv6z_PRFzAWSQq5XER_Uh8bcgvvMHE8pAlvGoU2lvZc')
+    window.open('https://mega.nz/#!SColGA6Q!rIZReM-XSJEG2-PjbY915lrsfBSGL0HfMMYBtWn5Lvg')
 }
 
-function openSourceCode() {
-    window.open('https://github.com/donaldtrumpyyy/DEMJAR')
+for (let i = 0; i < 666; i++) {
+    console.log('sub 2 pewdiepie')
 }
